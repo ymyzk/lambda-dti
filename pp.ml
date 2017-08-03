28e2ac3accf46c25fab268ee21e71e7aa41386b9
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2017-08-03 14:53:59 +0900

    Improve GTLC.pp_exp

--- pp.ml
+++ pp.ml
@@ -18,33 +18,47 @@ let rec pp_ty ppf = function
   | TyVar x -> fprintf ppf "'x%d" x
   | TyInt -> pp_print_string ppf "int"
   | TyBool -> pp_print_string ppf "bool"
-  | TyFun (u1, u2) -> begin
-      match u1 with
-      | TyFun _ -> fprintf ppf "(%a) -> %a" pp_ty u1 pp_ty u2
-      | _ -> fprintf ppf "%a -> %a" pp_ty u1 pp_ty u2
-    end
+  | TyFun (u1, u2) as u ->
+      fprintf ppf "%a -> %a"
+        (with_paren gt_ty pp_ty u) u1
+        pp_ty u2
+
+let pp_binop ppf op =
+  pp_print_string ppf begin
+    match op with
+    | Plus -> "+"
+    | Mult -> "*"
+    | Lt -> "<"
+  end
 
 module GTLC = struct
   open Syntax.GTLC
+
+  let gt_exp e_up e = match e_up, e with
+    | AppExp _, _ -> true
+    | BinOp (Mult, _, _), BinOp (Plus, _, _) -> true
+    | BinOp (Mult, _, _), BinOp (Lt, _, _) -> true
+    | BinOp (Plus, _, _), BinOp (Lt, _, _) -> true
+    | _ -> false
 
   let rec pp_exp ppf = function
     | Var x -> pp_print_string ppf x
     | BConst b -> pp_print_bool ppf b
     | IConst i -> pp_print_int ppf i
-    | BinOp (op, e1, e2) ->
+    | BinOp (op, e1, e2) as e ->
         fprintf ppf "%a %a %a"
-          pp_exp e1
+          (with_paren gt_exp pp_exp e) e1
           pp_binop op
-          pp_exp e2
+          (with_paren gt_exp pp_exp e) e2
     | FunExp (x1, u1, e) ->
         fprintf ppf "fun (%s: %a) -> %a"
           x1
           pp_ty u1
           pp_exp e
-    | AppExp (e1, e2) ->
-        fprintf ppf "((%a) (%a))"
+    | AppExp (e1, e2) as e ->
+        fprintf ppf "%a %a"
           pp_exp e1
-          pp_exp e2
+          (with_paren gt_exp pp_exp e) e2
 end
 
 module CC = struct
