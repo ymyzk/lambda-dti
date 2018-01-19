c65df35d5e4bb6acd47e3d2a5eb180eeb3d0e752
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2018-01-19 16:56:51 +0900

    Add ignore to stdlib

--- lib/stdlib.ml
+++ lib/stdlib.ml
@@ -26,4 +26,23 @@ let env, tyenv =
     (env, tyenv)
     implementations
 
+let implementations = [
+  "let ignore x = ();;"
+]
+
+let env, tyenv =
+ List.fold_left
+    (fun (env, tyenv) str ->
+      let e = Parser.toplevel Lexer.main @@ Lexing.from_string str in
+      let tyenv, e, u = Typing.GTLC.type_of_program tyenv e in
+      let tyenv = Typing.GTLC.normalize_tyenv tyenv in
+      let e = Typing.GTLC.normalize_program e in
+      let _ = Typing.GTLC.normalize_type u in
+      let f, _ = Typing.GTLC.translate tyenv e in
+      let _ = Typing.CC.type_of_program tyenv f in
+      let env, _, _ = Eval.eval_program env f in
+      env, tyenv)
+    (env, tyenv)
+    implementations
+
 let pervasives = env, tyenv
