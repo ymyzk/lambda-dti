3fe4ba086612ce8185754756a9916b3673948e0d
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2018-01-20 15:35:08 +0900

    Add print functions to stdlib

--- lib/stdlib.ml
+++ lib/stdlib.ml
@@ -9,14 +9,32 @@ let is_some_type = tysc_of_ty @@ TyFun (TyDyn, TyBool)
 let is_some u = FunV (fun _ -> function
     | Tagged (t, _) when u = tag_to_ty t -> BoolV true
     | Tagged _ -> BoolV false
-    |_ -> raise @@ Stdlib_bug "untagged value"
+    | _ -> raise @@ Stdlib_bug "untagged value"
   )
+
+let lib_print_bool = FunV (fun _ -> function
+    | BoolV b -> print_string @@ string_of_bool b; UnitV
+    | _ -> raise @@ Stdlib_bug "print_bool: unexpected value"
+)
+
+let lib_print_int = FunV (fun _ -> function
+    | IntV i -> print_int i; UnitV
+    | _ -> raise @@ Stdlib_bug "print_int: unexpected value"
+)
+
+let lib_print_newline = FunV (fun _ -> function
+    | UnitV -> print_newline (); UnitV
+    | _ -> raise @@ Stdlib_bug "print_newline: unexpected value"
+)
 
 let implementations = [
   "is_bool", [], is_some TyBool, is_some_type;
   "is_int", [], is_some TyInt, is_some_type;
   "is_unit", [], is_some TyUnit, is_some_type;
   "is_fun", [], is_some (TyFun (TyDyn, TyDyn)), is_some_type;
+  "print_bool", [], lib_print_bool, tysc_of_ty @@ TyFun (TyBool, TyUnit);
+  "print_int", [], lib_print_int, tysc_of_ty @@ TyFun (TyInt, TyUnit);
+  "print_newline", [], lib_print_newline, tysc_of_ty @@ TyFun (TyUnit, TyUnit);
 ]
 
 let env, tyenv =
